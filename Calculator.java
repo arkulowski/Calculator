f84f78b8c0515392e07df1cd1110dc180252d1d7
arkulowski <arkulowski1@quinnipiac.edu>
2023-02-14 16:27:38 -0500

    0 error

--- Calculator.java
+++ Calculator.java
@@ -25,13 +25,14 @@ public class Calculator {
             System.out.println("The product of " + num1 + " and " + num2 + " is " + result);
         } else if (operation.equals("/")) {
             if (num2 == 0) {
-                System.out.println("Cannot divide by zero.");
+                System.out.println("Error: Divide by Zero");
+                return;
             } else {
-                result = (double) num1 / num2;
+                result = num1 / num2;
                 System.out.println("The quotient of " + num1 + " and " + num2 + " is " + result);
             }
         } else {
-            System.out.println("Invalid operation entered.");
+            System.out.println("Invalid operation. Please enter +, -, *, or /.");
         }
     }
 }
