35efb5aacb790753098039a35f01f02a055665d6
arkulowski <arkulowski1@quinnipiac.edu>
2023-02-14 16:15:58 -0500

    add or subtract

--- Calculator.java
+++ Calculator.java
@@ -1,22 +1,29 @@
 import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args){
+        System.out.print("Enter the first whole number: ");
+        int num1 = input.nextInt();
 
-    
+        System.out.print("Enter the second whole number: ");
+        int num2 = input.nextInt();
 
+        System.out.print("Do you want to add or subtract the numbers? Enter 'add' or 'subtract': ");
+        String operation = input.next();
 
-    Scanner input = new Scanner(System.in);
-
-    System.out.print("Enter the first whole number: ");
-    int num1 = input.nextInt();
-
-    System.out.print("Enter the second whole number: ");
-    int num2 = input.nextInt();
-
-    int sum = num1 + num2;
-    System.out.println("The result of adding the two numbers is: " + sum);
-  }
+        int result;
+        if (operation.equals("add")) {
+            result = num1 + num2;
+            System.out.println("The sum of " + num1 + " and " + num2 + " is " + result);
+        } else if (operation.equals("subtract")) {
+            result = num1 - num2;
+            System.out.println("The difference between " + num1 + " and " + num2 + " is " + result);
+        } else {
+            System.out.println("Invalid operation entered.");
+        }
+    }
 }
 
     
