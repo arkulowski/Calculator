cf64e00e987b04d82dea645ebbf6b2444b67d66d
arkulowski <arkulowski1@quinnipiac.edu>
2023-02-14 16:20:09 -0500

    * / operation

--- Calculator.java
+++ Calculator.java
@@ -10,16 +10,26 @@ public class Calculator {
         System.out.print("Enter the second whole number: ");
         int num2 = input.nextInt();
 
-        System.out.print("Do you want to add or subtract the numbers? Enter 'add' or 'subtract': ");
+        System.out.print("Enter the operation you want to perform (+, -, *, /): ");
         String operation = input.next();
 
-        int result;
-        if (operation.equals("add")) {
+        double result;
+        if (operation.equals("+")) {
             result = num1 + num2;
             System.out.println("The sum of " + num1 + " and " + num2 + " is " + result);
-        } else if (operation.equals("subtract")) {
+        } else if (operation.equals("-")) {
             result = num1 - num2;
             System.out.println("The difference between " + num1 + " and " + num2 + " is " + result);
+        } else if (operation.equals("*")) {
+            result = num1 * num2;
+            System.out.println("The product of " + num1 + " and " + num2 + " is " + result);
+        } else if (operation.equals("/")) {
+            if (num2 == 0) {
+                System.out.println("Cannot divide by zero.");
+            } else {
+                result = (double) num1 / num2;
+                System.out.println("The quotient of " + num1 + " and " + num2 + " is " + result);
+            }
         } else {
             System.out.println("Invalid operation entered.");
         }
